14398a016c67d22fd826f532963db4fb8fa784d2
Foxterino <foxterinogg@gmail.com>
2020-09-18 18:06:08 +0300

    Added logout button

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Button } from 'antd';
+import { useDispatch } from 'react-redux';
+import { signOut } from '../State/Ducks/Auth';
 
-const Header: React.FC = () => <header>Header</header>;
+const Header: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const handleSignOut = () => dispatch(signOut());
+
+  return (
+    <header>
+      Header
+      <Button type="link" onClick={handleSignOut}>
+        Logout
+      </Button>
+    </header>
+  );
+};
 
 export { Header };
